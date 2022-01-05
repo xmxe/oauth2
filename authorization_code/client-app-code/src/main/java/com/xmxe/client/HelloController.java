af23d5e6c86a1cf3319c172e89a48b375b0817b9
xmxe <464817304@qq.com>
2022-01-05 16:58:58 +0800

    使用refresh_token刷新access_token

--- authorization_code/client-app-code/src/main/java/com/xmxe/client/HelloController.java
+++ authorization_code/client-app-code/src/main/java/com/xmxe/client/HelloController.java
@@ -32,7 +32,15 @@ public class HelloController {
             // 访问授权中心 携带授权码获取token
             Map<String,String> resp = restTemplate.postForObject("http://localhost:8080/oauth/token", map, Map.class);
             String access_token = resp.get("access_token");
-            System.out.println("access_token="+access_token);
+            String refresh_token = resp.get("refresh_token");
+            System.out.println("access_token="+access_token+"refresh_token="+refresh_token);
+
+            // 根据refresh_token刷新access_token --start 测试客户端模式resp没有返回refresh_token
+            // map.add("grant_type","refresh_token");
+            // map.add("refresh_token",refresh_token);
+            // Map<String,String> resp2 = restTemplate.postForObject("http://localhost:8080/oauth/token", map, Map.class);
+            // 根据refresh_token刷新access_token --end
+
             HttpHeaders headers = new HttpHeaders();
             headers.add("Authorization", "Bearer " + access_token);
             HttpEntity<Object> httpEntity = new HttpEntity<>(headers);
@@ -43,3 +51,11 @@ public class HelloController {
         return "index";
     }
 }
+/**
+ * /oauth/authorize	这个是授权的端点
+ * /oauth/token	这个是用来获取令牌的端点
+ * /oauth/confirm_access	用户确认授权提交的端点（就是 auth-server 询问用户是否授权那个页面的提交地址）
+ * /oauth/error	授权出错的端点
+ * /oauth/check_token	校验 access_token 的端点
+ * /oauth/token_key	提供公钥的端点
+ */
